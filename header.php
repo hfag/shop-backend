d8d7bf4f8e9505936edae5be6c95f0bf2105b3d3
Nico Hauser <me@tyratox.ch>
2018-09-16 11:25:54 +0200

    Redirect requests to the new shop

--- header.php
+++ header.php
@@ -1,3 +1,16 @@
+<?php
+	$host = $_SERVER['HTTP_HOST']; 
+
+	$hosts = array(
+		'shop.feuerschutz.ch',
+	);
+	
+	if($host !== "shop.feuerschutz.ch") {
+	    header('Location: https://shop.feuerschutz.ch');
+	}
+	
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
