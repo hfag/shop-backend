0e3c06d3b0b74dc3d210efea6b88172f547d2bd6
Nico Hauser <me@tyratox.ch>
2019-07-28 12:11:06 +0200

    Prevent emails from getting sent in english

--- woocommerce/emails/customer-new-account.php
+++ woocommerce/emails/customer-new-account.php
@@ -26,14 +26,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 <?php /* translators: %s Customer first name */ ?>
 <p><?php printf( esc_html__( 'Hi %s,', 'woocommerce' ), esc_html( $user_login ) ); ?></p>
 <?php /* translators: %1$s: Site title, %2$s: Username, %3$s: My account link */ ?>
-<p><?php printf( __( 'Thanks for creating an account on %1$s. As a reminder, the username you chose is %2$s. You can access your account area to view orders, change your password, and more at: %3$s', 'woocommerce' ), esc_html( $blogname ), '<strong>' . esc_html( $user_login ) . '</strong>', make_clickable( esc_url( "https://shop.feuerschutz.ch/konto" ) ) ); ?></p><?php // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped ?>
+<p>
+	Vielen Dank für die Erstellung eines Kontos im Shop der Hauser Feuerschutz AG. Ihr Benutzername lautet <?php echo esc_html( $user_login ) ?>.
+	Unter folgendem Link können Sie auf Ihr Konto zugreifen, um Bestellungen anzuzeigen, Ihr Passwort zu ändern usw.: <a href="https://shop.feuerschutz.ch/konto/">https://shop.feuerschutz.ch/konto/</a>
+</p>
 
-<?php if ( 'yes' === get_option( 'woocommerce_registration_generate_password' ) && $password_generated ) : ?>
-	<?php /* translators: %s Auto generated password */ ?>
-	<p><?php printf( esc_html__( 'Your password has been automatically generated: %s', 'woocommerce' ), '<strong>' . esc_html( $user_pass ) . '</strong>' ); ?></p>
-<?php endif; ?>
 
-<p><?php esc_html_e( 'We look forward to seeing you soon.', 'woocommerce' ); ?></p>
+<p>Wir freuen uns darauf, bald von Ihnen zu hören.</p>
 
 <?php
 //do_action( 'woocommerce_email_footer', $email );
